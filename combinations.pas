ec0200c1cffab075aaca8408682adf2107af0346
whekin <stanislavkalishin@gmail.com>
2018-10-31 22:45:18 +0300

    style the code

--- combinations.pas
+++ combinations.pas
@@ -3,15 +3,15 @@ const
     generalTotalityLength: integer = 4;
     amountOfSampling: integer = 2;
 var
-    generalTotality: array [1..generalTotalityLength] of integer;
+    generalTotality: array [0..generalTotalityLength - 1] of integer;
     i, j, k: integer;
 begin
-    for i := 1 to generalTotalityLength do
+    for i := 0 to generalTotalityLength - 1 do
         read(generalTotality[i]);
     
-    for i := 1 to generalTotalityLength - 1 do
+    for i := 0 to generalTotalityLength - 2 do
     begin
-        for j := i to generalTotalityLength - amountOfSampling + 1 do
+        for j := i to generalTotalityLength - amountOfSampling do
         begin
             for k := 0 to amountOfSampling - 2 do
                 write(generalTotality[i + k]);
