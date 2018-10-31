bdada2986ea3c7b0c6ef2baa09d6d41a0ab4e0d9
whekin <stanislavkalishin@gmail.com>
2018-10-31 20:33:33 +0300

    add files

---
+++ combinations.pas
@@ -0,0 +1,21 @@
+﻿program combinations;
+//ToDo: задается 4 числа, программа выводит все сочетания из двух элементов
+uses factorial; // fact
+
+const
+    generalTotalityLength: integer = 4;
+var
+    generalTotality: array [0..generalTotalityLength - 1] of integer;
+    quantity: integer;
+begin
+    quantity := fact(generalTotalityLength)
+                div (fact(2) * fact(generalTotalityLength - 2));
+    for var i := 0 to generalTotalityLength - 1 do
+        read(generalTotality[i]);
+    
+    for var i := 0 to generalTotalityLength - 2 do
+    begin
+        for var j := i to generalTotalityLength - 1 do
+            writeln(generalTotality[i], generalTotality[j]);
+    end;
+end.
---
+++ factorial.pas
@@ -0,0 +1,14 @@
+﻿unit factorial;
+interface
+function fact(number: integer): integer;
+
+implementation
+function fact(number: integer): integer;
+begin
+    if number = 1 then
+        fact := 1
+    else
+        fact := number * fact(number - 1);
+end;
+
+end.
